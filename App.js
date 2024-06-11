a1de723b1b43f0e12d0d6c1aba6472c7de91a789
Saransh2705 <153087349+Saransh2705@users.noreply.github.com>
2024-06-11 18:29:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,77 @@
+const Stack = createNativeStackNavigator();
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import LoginSignupPage from "./screens/LoginSignupPage";
+import Group from "./screens/Group";
+import Group1 from "./screens/Group1";
+import Group2 from "./screens/Group2";
+import Group3 from "./screens/Group3";
+import Group4 from "./screens/Group4";
+import MYBUSPNG from "./components/MYBUSPNG";
+import Ellipse from "./components/Ellipse";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
+
+  const [fontsLoaded, error] = useFonts({
+    "Inter-Medium": require("./assets/fonts/Inter-Medium.ttf"),
+    "Inter-SemiBold": require("./assets/fonts/Inter-SemiBold.ttf"),
+    "Nunito-Medium": require("./assets/fonts/Nunito-Medium.ttf"),
+    "Nunito-SemiBold": require("./assets/fonts/Nunito-SemiBold.ttf"),
+    "Nunito-Bold": require("./assets/fonts/Nunito-Bold.ttf"),
+    "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+    "Montserrat-Medium": require("./assets/fonts/Montserrat-Medium.ttf"),
+    "Oxygen-Regular": require("./assets/fonts/Oxygen-Regular.ttf"),
+    "Oxygen-Bold": require("./assets/fonts/Oxygen-Bold.ttf"),
+  });
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return (
+    <>
+      <NavigationContainer>
+        {hideSplashScreen ? (
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+              name="LoginSignupPage"
+              component={LoginSignupPage}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Group"
+              component={Group}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Group1"
+              component={Group1}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Group2"
+              component={Group2}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Group3"
+              component={Group3}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Group4"
+              component={Group4}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        ) : null}
+      </NavigationContainer>
+    </>
+  );
+};
+export default App;
